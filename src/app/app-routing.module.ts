7139eaa220b88eef983cb3a666877441171d1f79
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-15 17:38:51 +0200

    user
    route to userModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './modules/user/services/authguard.service';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'folder/Inbox',
     pathMatch: 'full'
-  },
+  },,
+  {path:"user",
+  
+  loadChildren:()=>import('./modules/user/user.module').then(m=>m.UserModule)
+},
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
