ff4d56d7dd57f08cf8d46d8641542c410ec3128d
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-15 14:46:04 +0200

    hello world cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,18 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req:any, res:any) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've successfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+  });
