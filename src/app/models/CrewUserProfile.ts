36db04c70724b9e6a698683b86acd52135b2a45d
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-20 17:16:02 +0200

    implementing Categories module

--- src/app/models/CrewUserProfile.ts
+++ src/app/models/CrewUserProfile.ts
@@ -1,4 +1,12 @@
-export enum UserKInd{
-    worker=1,
+import { UserModel } from "../modules/user/models/userModel";
+
+export enum UserKInd {
+    worker = 1,
     enterprise
+}
+
+export class CrewUserprofile extends UserModel {
+
+type:UserKInd
+categories
 }
