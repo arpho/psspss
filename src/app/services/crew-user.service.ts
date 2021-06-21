59b00cb3782363d1a8f3a05192fb4eb5c8fdf332
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-21 16:27:52 +0200

    implementing crewUserService
    implemented getItem and updateItem

--- src/app/services/crew-user.service.ts
+++ src/app/services/crew-user.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import  * as firebase from 'firebase';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { ItemModelInterface } from '../modules/item/models/itemModelInterface';
+import { ItemServiceInterface } from '../modules/item/models/ItemServiceInterface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CrewUserService {
-
+export class CrewUserService implements ItemServiceInterface {
+public UserListRef:firebase.default.database.Reference
   constructor() { }
+
+  suppliersListRef?: any;
+  _items: BehaviorSubject<ItemModelInterface[]>;
+  items_list: ItemModelInterface[];
+  items: Observable<ItemModelInterface[]>;
+  getItem(key: string): firebase.default.database.Reference {
+    throw new Error('Method not implemented.');
+  }
+  updateItem(item: ItemModelInterface) {
+    throw new Error('Method not implemented.');
+  }
+  deleteItem(key: string) {
+    throw new Error('Method not implemented.');
+  }
+  getDummyItem(): ItemModelInterface {
+    throw new Error('Method not implemented.');
+  }
+  createItem(item: ItemModelInterface) {
+    throw new Error('Method not implemented.');
+  }
 }
