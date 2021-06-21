f9f89566dd2347d4fd8cc766117d44a95178f7df
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-21 18:16:13 +0200

    implementing
    service is completed#TODO: to be verified

--- src/app/services/crew-user.service.ts
+++ src/app/services/crew-user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import  * as firebase from 'firebase';
+import * as firebase from 'firebase';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { CrewUserprofileModel } from '../models/CrewUserProfile';
 import { ItemModelInterface } from '../modules/item/models/itemModelInterface';
@@ -10,31 +10,36 @@ import { ItemServiceInterface } from '../modules/item/models/ItemServiceInterfac
 })
 export class CrewUserService implements ItemServiceInterface {
 
-public UserListRef:firebase.default.database.Reference
-  constructor() { 
+  
 
-    firebase.default.auth().onAuthStateChanged(user=>{
-      if(user){
-        this.suppliersListRef = firebase.default.database().ref("/userprofiles")
-        this.UserListRef.on("value",()=>{
-          this.items_list =[]
-        })
-      }
-    })
+  public UserListRef: firebase.default.database.Reference
+  _items: BehaviorSubject<Array<CrewUserprofileModel>> = new BehaviorSubject([])
+  readonly items: Observable<Array<CrewUserprofileModel>> = this._items.asObservable()
+  items_list: Array<CrewUserprofileModel> = []
+  constructor() {
+
+   
   }
-  categoriesService?: ItemServiceInterface;
-  suppliersService?: ItemServiceInterface;
-  paymentsService?: ItemServiceInterface;
+
   initializeItems() {
-    throw new Error('Method not implemented.');
+    firebase.default.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.UserListRef = firebase.default.database().ref('/userProfile');
+        this.UserListRef.on('value', eventCategoriesListSnapshot => {
+          this.items_list = [];
+          eventCategoriesListSnapshot.forEach(snap => {
+            const userProfile = new CrewUserprofileModel().initialize(snap.val())
+            this.items_list.push(userProfile);
+          });
+          this._items.next(this.items_list)
+        });
+      }
+    });
   }
 
   suppliersListRef?: any;
-  _items: BehaviorSubject<CrewUserprofileModel[]>;
-  items_list: CrewUserprofileModel[];
-  items: Observable<CrewUserprofileModel[]>;
   getItem(key: string): firebase.default.database.Reference {
-   return this.UserListRef.child(key)
+    return this.UserListRef.child(key)
   }
   updateItem(item: ItemModelInterface) {
     return this.UserListRef.child(item.key).update(item.serialize())
@@ -48,9 +53,10 @@ public UserListRef:firebase.default.database.Reference
   async createItem(item: CrewUserprofileModel) {
     var User = new CrewUserprofileModel()
     const user = await this.UserListRef.push(item.serialize())
-    user.on("value", item=>{User.initialize(item.val() );
-    User.key = item.key;
-    this.updateItem(User) //add
+    user.on("value", item => {
+      User.initialize(item.val());
+      User.key = item.key;
+      this.updateItem(User) //add
     })
   }
 }
