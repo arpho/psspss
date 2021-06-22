772e5db7442e60b0b3e2ba6a92458206f9cdfd16
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-22 09:44:00 +0200

    update-crew-user-page
    the update page now extends  the create user page

--- src/app/pages/create-crew-user/create-crew-user.page.ts
+++ src/app/pages/create-crew-user/create-crew-user.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCrewUserPage implements OnInit {
 
+  title = "nuovo Utente"
+
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/update-crew-user/update-crew-user.page.ts
+++ src/app/pages/update-crew-user/update-crew-user.page.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-update-crew-user',
-  templateUrl: './update-crew-user.page.html',
-  styleUrls: ['./update-crew-user.page.scss'],
+  templateUrl: '../create-crew-user/create-crew-user.page.html',
+  styleUrls: ['../create-crew-user/create-crew-user.page.scss'],
 })
 export class UpdateCrewUserPage implements OnInit {
+
+  title = " modifica Utente"
 
   constructor() { }
 
