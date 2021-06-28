34aec579b3144539c38720f4583702960a98bd4d
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-28 11:47:14 +0200

    refactoring
    implemented field

--- src/app/pages/create-crew-user/create-crew-user.page.ts
+++ src/app/pages/create-crew-user/create-crew-user.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { CrewUserprofileModel } from 'src/app/models/CrewUserProfile';
+import { ActivityFields } from 'src/app/models/enumFields';
 import { QuestionBase } from 'src/app/modules/dynamic-form/models/question-base';
 import { DateQuestion } from 'src/app/modules/dynamic-form/models/question-date';
+import { DropdownQuestion } from 'src/app/modules/dynamic-form/models/question-dropdown';
 import { TextboxQuestion } from 'src/app/modules/item/models/question-textbox';
 import { CrewUserService } from 'src/app/services/crew-user.service';
 
@@ -15,13 +17,20 @@ export class CreateCrewUserPage implements OnInit {
   userprofile = new CrewUserprofileModel()
   title = "nuovo Utente"
   userFields: Array<any>
+  activityFields = [
+    {key:"sala",value:ActivityFields.sala},
+    {key:"accoglienza",value:ActivityFields.accoglienza},
+    {key:"pasticceria",value:ActivityFields.pasticceria},
+    {key:"cucina",value:ActivityFields.cucina}
+]
 
   constructor(public service: CrewUserService,public modalCtrl:ModalController) {
     this.userFields = [
       new TextboxQuestion({ key: 'firstName', label: 'Nome', value: this.userprofile.firstName }),
       new TextboxQuestion({ key: 'lastName', label: 'Cognome', value: this.userprofile.lastName }),
       new DateQuestion({ key: "birthDate", label: "Data di nascita", value: this.userprofile.birthDate.formatDate() }),
-      new TextboxQuestion({ key: "crewRole", label: "mansione", })
+      new TextboxQuestion({ key: "crewRole", label: "mansione", }),
+      new DropdownQuestion({key:"field",options:this.activityFields,label:"indirizzo"  })
     ]
   }
 
