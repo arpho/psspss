e9dc2a4f71558fb5d835b60f300d96d063dd1fa3
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-29 19:01:18 +0200

    refactoring
    defined filterFunction and sorterFunction

--- src/app/modules/user/pages/users/users.page.ts
+++ src/app/modules/user/pages/users/users.page.ts
@@ -3,6 +3,7 @@ import { UsersService } from "../../services/users.service";
 import { ItemModelInterface } from "src/app/modules/item/models/itemModelInterface";
 import { UserModel } from "../../models/userModel";
 import { EditUserPage } from "../edit-user/edit-user.page"
+import { CrewUserprofileModel } from "src/app/models/CrewUserProfile";
 
 @Component({
   selector: "app-users",
@@ -10,6 +11,8 @@ import { EditUserPage } from "../edit-user/edit-user.page"
   styleUrls: ["./users.page.scss"]
 })
 export class UsersPage implements OnInit {
+  public filterFunction: (item: CrewUserprofileModel) => boolean;
+  public sorterFunction: (a: any, b: any) => number
   public usersList: Array<ItemModelInterface>;
   public
   editModalPage = EditUserPage
