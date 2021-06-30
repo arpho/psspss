0c03251d08227c187cbe2a3e15f1111c75c07eae
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-30 17:46:23 +0200

    refactoring
    corrected typo, I added ' in ngSwitchCase

--- src/app/modules/dynamic-form/models/question-picture.ts
+++ src/app/modules/dynamic-form/models/question-picture.ts
@@ -1,7 +1,7 @@
 import { TextboxQuestion } from "../../item/models/question-textbox";
 import { QuestionProperties } from "./questionproperties";
 
-export class ictureBox extends TextboxQuestion{
+export class PictureBox extends TextboxQuestion{
     constructor(options:QuestionProperties<string>){
         super(options)
         this.controlType = "pictureBox"
--- src/app/pages/create-crew-user/create-crew-user.page.ts
+++ src/app/pages/create-crew-user/create-crew-user.page.ts
@@ -5,6 +5,7 @@ import { ActivityFields } from 'src/app/models/enumFields';
 import { QuestionBase } from 'src/app/modules/dynamic-form/models/question-base';
 import { DateQuestion } from 'src/app/modules/dynamic-form/models/question-date';
 import { DropdownQuestion } from 'src/app/modules/dynamic-form/models/question-dropdown';
+import { PictureBox } from 'src/app/modules/dynamic-form/models/question-picture';
 import { TextboxQuestion } from 'src/app/modules/item/models/question-textbox';
 import { CrewUserService } from 'src/app/services/crew-user.service';
 
@@ -27,6 +28,7 @@ export class CreateCrewUserPage implements OnInit {
 
   constructor(public service: CrewUserService,public modalCtrl:ModalController) {
     this.userFields = [
+      new PictureBox({key:"picture", label:"fotazza"}),
       new TextboxQuestion({ key: 'firstName', label: 'Nome', value: this.userprofile.firstName }),
       new TextboxQuestion({ key: 'lastName', label: 'Cognome', value: this.userprofile.lastName }),
       new DateQuestion({ key: "birthDate", label: "Data di nascita", value: this.userprofile.birthDate.formatDate() }),
