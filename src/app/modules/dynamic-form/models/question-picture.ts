3dad3fc41fff209d3bd27b1193138d7ed67a88d6
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-30 17:28:23 +0200

    pictureQuestion
    implemented new model

---
+++ src/app/modules/dynamic-form/models/question-picture.ts
@@ -0,0 +1,8 @@
+import { TextboxQuestion } from "../../item/models/question-textbox";
+import { QuestionProperties } from "./questionproperties";
+
+export class ictureBox extends TextboxQuestion{
+    constructor(options:QuestionProperties<string>){
+        super(options)
+    }
+}
