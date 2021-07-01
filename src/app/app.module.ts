4e7f5596175182af146d0cc1d23d2761ac891171
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-01 12:38:02 +0200

    explolring
     installed FileChooser: works only with cordova dos not open not even in the device

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { SelectorItemsPage } from './modules/item/pages/selector-items/selector-
 import { CreateCrewUserPage } from './pages/create-crew-user/create-crew-user.page';
 import { UpdateCrewUserPage } from './pages/update-crew-user/update-crew-user.page';
 import { EditUserPage } from './modules/user/pages/edit-user/edit-user.page';
-import { ImagePicker } from '@ionic-native/image-picker/ngx';
+import { FileChooser } from '@ionic-native/file-chooser/ngx';
 
 @NgModule({
   declarations: [AppComponent,
@@ -31,8 +31,7 @@ import { ImagePicker } from '@ionic-native/image-picker/ngx';
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,ItemModule,DynamicFormModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
-    ImagePicker
-    
+    FileChooser
     
     
   ],
--- src/app/modules/dynamic-form/components/file-picker/file-picker.component.ts
+++ src/app/modules/dynamic-form/components/file-picker/file-picker.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ImagePicker, ImagePickerOptions } from '@ionic-native/image-picker/ngx';
+import { FileChooser } from '@ionic-native/file-chooser/ngx';
 @Component({
   selector: 'app-file-picker',
   templateUrl: './file-picker.component.html',
@@ -7,15 +7,14 @@ import { ImagePicker, ImagePickerOptions } from '@ionic-native/image-picker/ngx'
 })
 export class FilePickerComponent implements OnInit {
 
-  constructor(private imagePicker:ImagePicker) { }
+  constructor(private fileChooser:FileChooser) { }
   chooseFile(){
-    const options:ImagePickerOptions={maximumImagesCount:1,
-    allow_video:false,
-  title:"picture"}
+ 
     console.log('ciao')
-    this.imagePicker.getPictures(options
-      
-    ).then(uri=>console.log('picture',uri)).catch(e=>console.error(e))
+    this.fileChooser.open().
+    then(uri=>console.log(uri? uri:'canceled')).
+    catch(e=>console.error(e))
+    
   }
 
   ngOnInit() {}
