65e5626e7078c2e4484610a72b87841c22943f0c
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-01 12:48:18 +0200

    refactoring
    removed  filechooser

--- src/app/modules/dynamic-form/components/file-picker/file-picker.component.ts
+++ src/app/modules/dynamic-form/components/file-picker/file-picker.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FileChooser } from '@ionic-native/file-chooser/ngx';
+
 @Component({
   selector: 'app-file-picker',
   templateUrl: './file-picker.component.html',
   styleUrls: ['./file-picker.component.scss'],
 })
 export class FilePickerComponent implements OnInit {
+  
 
-  constructor(private fileChooser:FileChooser) { }
+  constructor() { }
   chooseFile(){
  
     console.log('ciao')
-    this.fileChooser.open().
-    then(uri=>console.log(uri? uri:'canceled')).
-    catch(e=>console.error(e))
+ 
     
   }
 
