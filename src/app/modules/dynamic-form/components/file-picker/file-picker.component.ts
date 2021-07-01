34ace1b225d36be56b9c2d83893c0f229f387981
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-01 10:55:28 +0200

    refactoring
    changed icon from aperture to folder and added console.log

--- src/app/modules/dynamic-form/components/file-picker/file-picker.component.ts
+++ src/app/modules/dynamic-form/components/file-picker/file-picker.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class FilePickerComponent implements OnInit {
 
   constructor() { }
+  chooseFile(){
+    console.log('ciao')
+  }
 
   ngOnInit() {}
 
