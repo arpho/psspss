70800f3ecb2c8e650b2852681df11ab787db6671
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-01 16:18:09 +0200

    refactoring
    declared components in categoriesModule

--- src/app/modules/categories/categories.module.ts
+++ src/app/modules/categories/categories.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CategoriesSelectorComponent } from './components/categories-selector/categories-selector.component';
+import { CategoriesViewerComponent } from './components/categories-viewer/categories-viewer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CategoriesSelectorComponent,CategoriesViewerComponent],
   imports: [
     CommonModule
   ]
--- src/app/pages/create-crew-user/create-crew-user.module.ts
+++ src/app/pages/create-crew-user/create-crew-user.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
-import { CreateCrewUserPageRoutingModule } from './create-crew-user-routing.module';
-
 import { CreateCrewUserPage } from './create-crew-user.page';
 import { DynamicFormModule } from 'src/app/modules/dynamic-form/dynamic-form.module';
+import { CategoryModel } from 'src/app/modules/categories/models/CategoryModel';
 
 @NgModule({
   imports: [
@@ -15,8 +12,8 @@ import { DynamicFormModule } from 'src/app/modules/dynamic-form/dynamic-form.mod
     FormsModule,
     IonicModule,
     DynamicFormModule,
-    CreateCrewUserPageRoutingModule
+    CategoryModel,
   ],
   declarations: [CreateCrewUserPage]
 })
-export class CreateCrewUserPageModule {}
+export class CreateCrewUserPageModule { }
