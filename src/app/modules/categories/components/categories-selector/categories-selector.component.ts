8685399574e418dafa6d4ae00c0d94a25b79eedc
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-12 11:04:45 +0200

    implemantation
    implemented interface ControlValueAccessor

--- src/app/modules/categories/components/categories-selector/categories-selector.component.ts
+++ src/app/modules/categories/components/categories-selector/categories-selector.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter
 import { ModalController } from '@ionic/angular';
 import { CategoriesSelectorPage } from '../../pages/categories-selector/categories-selector.page';
 import { CategoryModel } from '../../models/CategoryModel';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-categories-selector',
@@ -10,15 +11,32 @@ import { CategoryModel } from '../../models/CategoryModel';
   styleUrls: ['./categories-selector.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CategoriesSelectorComponent implements OnInit {
+export class CategoriesSelectorComponent implements OnInit,ControlValueAccessor {
   @Input() categoriesList: Array<CategoryModel>
   @Input() buttonText = "categorie"
   @Output() selectedCategories: EventEmitter<Array<CategoryModel>> = new EventEmitter()
   categoryColor = 'orange'
   categoryIcon = 'pricetag'
+  // tslint:disable-next-line: ban-types
+  onChange: any = () => { };
+  // tslint:disable-next-line: ban-types
+  onTouched: any = () => { };
+  disabled = false
 
 
   constructor(public modalCtrl: ModalController) { }
+  writeValue(obj: any): void {
+    this.categoriesList = obj
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn
+  }
+  setDisabledState?(disabled: boolean): void {
+    this.disabled = disabled
+  }
 
   ngOnInit() {
     this.buttonText = this.buttonText || "categorie"
