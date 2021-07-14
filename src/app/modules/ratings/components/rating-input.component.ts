623ca891ba0552a0f97dcf58f8010314e9cc41dc
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-14 10:40:14 +0200

    rating-input
    generated component

---
+++ src/app/modules/ratings/components/rating-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rating-input',
+  templateUrl: './rating-input.component.html',
+  styleUrls: ['./rating-input.component.scss'],
+})
+export class RatingInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
