f398aadde8d2203c5a5141a860a1244355c08d19
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-14 10:45:48 +0200

    rating-input
    implemented logic

--- src/app/modules/ratings/components/rating-input.component.ts
+++ src/app/modules/ratings/components/rating-input.component.ts
@@ -11,4 +11,16 @@ export class RatingInputComponent implements OnInit {
 
   ngOnInit() {}
 
+  stars: boolean[] = Array(5).fill(false);
+
+  get value(): number {
+    return this.stars.reduce((total, starred) => {
+      return total + (starred ? 1 : 0);
+    }, 0);
+  }
+
+  rate(rating: number) {
+    this.stars = this.stars.map((_, i) => rating > i);
+  }
+
 }
