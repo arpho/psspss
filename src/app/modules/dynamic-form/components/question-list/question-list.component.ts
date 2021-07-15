4a433e71ab88c4b655d6c517aac02148e4be6ab6
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-15 13:04:08 +0200

    implemented controlValueAccesor

--- src/app/modules/dynamic-form/components/question-list/question-list.component.ts
+++ src/app/modules/dynamic-form/components/question-list/question-list.component.ts
@@ -15,19 +15,25 @@ import { ListableItemInterface } from '../../models/listableItemInterface';
 })
 export class QuestionListComponent implements OnInit, ControlValueAccessor {
   @Input() itemsList: Array<ListableItemInterface>
+  // tslint:disable-next-line: ban-types
+  onChange: any = () => { };
+  // tslint:disable-next-line: ban-types
+  onTouched: any = () => { };
+  disabled = false
 
   constructor() { }
   writeValue(obj: any): void {
     this.itemsList = obj
+    this.onChange(obj)
   }
   registerOnChange(fn: any): void {
-    throw new Error('Method not implemented.');
+    this.onChange = fn
   }
   registerOnTouched(fn: any): void {
-    throw new Error('Method not implemented.');
+    this.onTouched = fn
   }
   setDisabledState?(isDisabled: boolean): void {
-    throw new Error('Method not implemented.');
+    this.disabled = isDisabled
   }
 
   ngOnInit() { }
