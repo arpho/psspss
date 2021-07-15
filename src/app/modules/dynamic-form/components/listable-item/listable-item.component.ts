260962c22a7b523a06786bfc43319d8d90f313bc
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-15 12:01:29 +0200

    listableItemComponent
    generated component

---
+++ src/app/modules/dynamic-form/components/listable-item/listable-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listable-item',
+  templateUrl: './listable-item.component.html',
+  styleUrls: ['./listable-item.component.scss'],
+})
+export class ListableItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
