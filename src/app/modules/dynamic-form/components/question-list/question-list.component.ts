0bb868ea870f1c248cfefc946dbe16e966a1823b
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-15 12:59:19 +0200

    implemented list and controlAccessorValue
    implemented ionic-list on question-list.component

--- src/app/modules/dynamic-form/components/question-list/question-list.component.ts
+++ src/app/modules/dynamic-form/components/question-list/question-list.component.ts
@@ -1,16 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ListableItemInterface } from '../../models/listableItemInterface';
 
 @Component({
   selector: 'app-question-list',
   templateUrl: './question-list.component.html',
   styleUrls: ['./question-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    multi: true,
+    useExisting: forwardRef(() => QuestionListComponent)
+  }]
 })
-export class QuestionListComponent implements OnInit {
-  @Input() itemsList:Array<ListableItemInterface>
+export class QuestionListComponent implements OnInit, ControlValueAccessor {
+  @Input() itemsList: Array<ListableItemInterface>
 
   constructor() { }
+  writeValue(obj: any): void {
+    this.itemsList = obj
+  }
+  registerOnChange(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
