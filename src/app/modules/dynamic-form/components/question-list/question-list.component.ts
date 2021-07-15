6f5d780b2535783866e6418ebe6678530515ec63
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-15 12:15:55 +0200

    implementation
    created  input field in question-list component

--- src/app/modules/dynamic-form/components/question-list/question-list.component.ts
+++ src/app/modules/dynamic-form/components/question-list/question-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ListableItemInterface } from '../../models/listableItemInterface';
 
 @Component({
   selector: 'app-question-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-list.component.scss'],
 })
 export class QuestionListComponent implements OnInit {
+  @Input() itemsList:Array<ListableItemInterface>
 
   constructor() { }
 
