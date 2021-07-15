ea327d075a28b68e4520df73c714d9ec9cb4c345
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-15 11:47:49 +0200

    refactoring
    added comment

--- src/app/modules/item/models/listableInterface.ts
+++ src/app/modules/item/models/listableInterface.ts
@@ -1,6 +1,6 @@
 import { Value } from "./value";
 
-export interface ListableInterface {
+export interface ListableInterface { /**nedded to list item in listComponent */
     leftField: Value
     middlefield: Value
     rightField: Value
