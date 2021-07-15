2dce0fb416fdf364f92a0eef18980f2781e66f2e
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-15 12:07:23 +0200

    refactoring
    set new path for import of Value

--- src/app/modules/dynamic-form/models/listableItemInterface.ts
+++ src/app/modules/dynamic-form/models/listableItemInterface.ts
@@ -1,4 +1,4 @@
-import { Value } from "./value";
+import { Value } from "../../item/models/value";
 
 export interface ListableItemInterface { /**nedded to list item in listComponent */
     leftField: Value
