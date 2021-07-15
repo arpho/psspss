b1ed761c128c9937a6777b3ecc59bb61cc034a31
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-15 19:37:36 +0200

    declared page in ratingsModule

--- src/app/modules/ratings/ratings.module.ts
+++ src/app/modules/ratings/ratings.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RatingInputComponent } from './components/rating-input.component';
+import { CreateReviewPage } from './pages/create-review/create-review.page';
 
 
 
 @NgModule({
-  declarations: [RatingInputComponent],
+  declarations: [RatingInputComponent,CreateReviewPage],
   imports: [
     CommonModule
   ],
