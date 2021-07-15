2c17dd5b183858787d09e1d4dff302491195a98a
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-15 19:26:57 +0200

    open modal page in question-list-component

--- src/app/modules/dynamic-form/components/question-list/question-list.component.ts
+++ src/app/modules/dynamic-form/components/question-list/question-list.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, Component, forwardRef, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component,  forwardRef, Input, OnInit } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
 import { ListableItemInterface } from '../../models/listableItemInterface';
+import { ComponentRef } from '@ionic/core';
 
 @Component({
   selector: 'app-question-list',
@@ -15,28 +18,39 @@ import { ListableItemInterface } from '../../models/listableItemInterface';
 })
 export class QuestionListComponent implements OnInit, ControlValueAccessor {
   @Input() itemsList: Array<ListableItemInterface> = []
+  @Input() createModalPage: ComponentRef
   // tslint:disable-next-line: ban-types
   onChange: any = () => { };
   // tslint:disable-next-line: ban-types
   onTouched: any = () => { };
   disabled = false
 
-  constructor() { }
+ngOnInit() { 
+  }
+
+  constructor(public modalController:ModalController) { }
   writeValue(obj: any): void {
     this.itemsList = obj
     this.onChange(obj)
   }
+
   registerOnChange(fn: any): void {
     this.onChange = fn
   }
+
   registerOnTouched(fn: any): void {
     this.onTouched = fn
   }
+
   setDisabledState?(isDisabled: boolean): void {
     this.disabled = isDisabled
   }
 
-  ngOnInit() { 
+  async createItem() {
+    const modal = await this.modalController.create({ component: this.createModalPage })
+    return await modal.present()
+
   }
 
+  
 }
