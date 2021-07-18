52ac646bdee1001729aa7e307e19f9b43b0345e0
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-18 11:33:09 +0200

    added filter for specialization

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -47,6 +47,7 @@ export class FolderPage implements OnInit {
     const filter4Mansione = (value: string, item: CrewUserprofileModel) => item.crewRole.toLocaleLowerCase().includes(value)
     const filter4Field = (value: number, item: CrewUserprofileModel) => item.field == value
     const filter4Skill = (value: CategoryModel, item: CrewUserprofileModel) => item.isSkilled(value.key)
+    
     this.filterFields = [
       new TextboxQuestion({
         key: "nome",
@@ -68,7 +69,11 @@ export class FolderPage implements OnInit {
         label: "settore",
         options: this.activityFields,
         filterFunction: filter4Field
-      }), new SelectorQuestion({ key: 'skill', label: "filtra per abilità", filterFunction: filter4Skill, service: this.categories, text: 'Abilità', createPopup: '' })
+      }), new SelectorQuestion({ key: 'skill', label: "filtra per abilità", filterFunction: filter4Skill, service: this.categories, text: 'Abilità', createPopup: '' }),
+      new DropdownQuestion({key:"field",
+      options:this.activityFields,
+      filterFunction:filter4Field,
+      label:"cerca per indirizzo"  }),
     ]
   }
 
