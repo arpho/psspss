f81de0c6b50a809330c14e69dac92f2c6b274e66
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-20 17:42:01 +0200

    displayingf page of

--- src/app/modules/item/components/paging-controller/paging-controller.component.ts
+++ src/app/modules/item/components/paging-controller/paging-controller.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { paginationConfig } from '../../models/paginationConfig';
 
 @Component({
@@ -7,13 +7,23 @@ import { paginationConfig } from '../../models/paginationConfig';
   styleUrls: ['./paging-controller.component.scss'],
 })
 export class PagingControllerComponent implements OnInit {
+  @Input() paginationConfig:paginationConfig
+  @Input() countItems:number
 
+  currentPage=0
+
+  display(){
+    return `pagina${this.currentPage} di ${Math.floor(this.countItems/this.paginationConfig.items4page)}`
+  }
  
 
   constructor() {
     console.log("ciao co")
    }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+
+  }
 
 }
