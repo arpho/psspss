873d6825d8a6fb1cb428d641301e922c20630054
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-20 16:13:56 +0200

    renamed pg to currentPage and set default =0

--- src/app/modules/item/pipes/pagination.pipe.ts
+++ src/app/modules/item/pipes/pagination.pipe.ts
@@ -10,13 +10,13 @@ import { paginationConfig } from '../models/paginationConfig';
 
 export class PaginationPipe implements PipeTransform {
 
-  getPage = (a: Array<ItemModelInterface>, pg: number, item4page = 5) => {
-    return a.slice((pg - 1) * item4page, pg * item4page)
+  getPage = (a: Array<ItemModelInterface>, currentPage = 0, item4page = 5) => {
+    return a.slice((currentPage - 1) * item4page, currentPage * item4page)
   }
 
   transform(allItems: ItemModelInterface[], args?: paginationConfig): unknown {
 
-    return args.paginationActive? this.getPage(allItems,args.currentPage,args.items4page):allItems
+    return args.paginationActive ? this.getPage(allItems, args.currentPage, args.items4page) : allItems
   }
 
 }
