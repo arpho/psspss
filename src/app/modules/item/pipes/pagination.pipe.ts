16045170797b856d68e4c8ffa144ce4c33b96243
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-20 11:45:54 +0200

    implemented pagination pipe e paginationConfig

--- src/app/modules/item/pipes/pagination.pipe.ts
+++ src/app/modules/item/pipes/pagination.pipe.ts
@@ -1,12 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { getPackedSettings } from 'http2';
+import { ItemModelInterface } from '../models/itemModelInterface';
+import { paginationConfig } from '../models/paginationConfig';
 
 @Pipe({
   name: 'pagination'
 })
+
+
 export class PaginationPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  getPage = (a: Array<ItemModelInterface>, pg: number, item4page = 5) => {
+    return a.slice((pg - 1) * item4page, pg * item4page)
+  }
+
+  transform(allItems: ItemModelInterface[], args?: paginationConfig): unknown {
+
+    return args.paginationActive? this.getPage(allItems,args.currentPage,args.items4page):allItems
   }
 
 }
