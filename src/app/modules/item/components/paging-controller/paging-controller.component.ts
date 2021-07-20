752d878f7785c995747c600b1a18feedb4e178b2
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-20 19:54:26 +0200

    we show a toast for wrong settings

--- src/app/modules/item/components/paging-controller/paging-controller.component.ts
+++ src/app/modules/item/components/paging-controller/paging-controller.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
 import { paginationConfig } from '../../models/paginationConfig';
 
 @Component({
@@ -8,75 +8,93 @@ import { paginationConfig } from '../../models/paginationConfig';
   styleUrls: ['./paging-controller.component.scss'],
 })
 export class PagingControllerComponent implements OnInit {
-  @Input() paginationConfig:paginationConfig
-  @Input() countItems:number
+  @Input() paginationConfig: paginationConfig
+  @Input() countItems: number
   @Output() currentPageEvent: EventEmitter<number> = new EventEmitter<number>()
-  @Output() items4Page:EventEmitter<number>= new EventEmitter<number>()
-  currentPage:number
+  @Output() items4Page: EventEmitter<number> = new EventEmitter<number>()
+  currentPage: number
 
-  constructor(private alertCtrl:AlertController) {
-   }
+  constructor(private alertCtrl: AlertController,
+    public toastController: ToastController) {
+  }
 
   ngOnInit() {
-this.currentPage =this.paginationConfig.currentPage
+    this.currentPage = this.paginationConfig.currentPage
 
   }
 
-  display(){
-    return `pag ${this.currentPage+1} di ${Math.floor(this.countItems/this.paginationConfig.items4page)}`
+  display() {
+    return `pag ${this.currentPage + 1} di ${Math.floor(this.countItems / this.paginationConfig.items4page)}`
   }
 
-  previousPage(){
-    if(this.currentPage>0){
-      this.currentPage-=1
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage -= 1
       this.currentPageEvent.emit(this.currentPage)
-  
+
     }
   }
-  nextPage(){
-    if(this.currentPage+1<this.numberOfPage()){
-      this.currentPage+=1
+  nextPage() {
+    if (this.currentPage + 1 < this.numberOfPage()) {
+      this.currentPage += 1
       this.currentPageEvent.emit(this.currentPage)
-  
+
     }
   }
 
-  firstPage(){
-    if(this.currentPage>0){
-      this.currentPage =0
+  firstPage() {
+    if (this.currentPage > 0) {
+      this.currentPage = 0
       this.currentPageEvent.emit(this.currentPage)
-  
+
     }
   }
-  lastPage(){
-    this.currentPage= this.numberOfPage()
+  lastPage() {
+    this.currentPage = this.numberOfPage()
     this.currentPageEvent.emit(this.currentPage)
   }
 
-  
 
-  numberOfPage(){
-    return Math.floor(this.countItems/this.paginationConfig.items4page)
+
+  numberOfPage() {
+    return Math.floor(this.countItems / this.paginationConfig.items4page)
   }
 
-  async settings(){
-    let alert =  await this.alertCtrl.create(
+  async showToast(msg:string){
+    const toast = await this.toastController.create({
+      message:msg,
+      duration:2000,position:"middle"
+    })
+    toast.present()
+  }
+
+  async settings() {
+    let alert = await this.alertCtrl.create(
       {
         cssClass: 'my-custom-class',
         header: 'impostazioni di paginazione',
         subHeader: 'quanti candidati per pagina?',
         message: `1-${this.countItems}`,
-        inputs:[{name:"items4Page",
-      type:'number'}],
+        inputs: [{
+          name: "items4Page",
+          type: 'number'
+        }],
         buttons: ['OK']
-      
-    })
-    alert.onDidDismiss().then(v=>{
-     this.firstPage() //we move to the first page
-      this.items4Page.emit(Number(v.data.values.items4Page))
-    })
+
+      })
+    alert.onDidDismiss().then(v => {
+      const items4Page = Number(v.data.values.items4Page)
+      if (items4Page > 0 && items4Page <= this.countItems) {
+        this.firstPage() //we move to the first page
+        this.items4Page.emit(Number(v.data.values.items4Page))
+      }
+      else{
+        this.showToast(`puoi inserire un valore compreso tra 1 e ${this.countItems}`)
+      }
+    }
+    )
     await alert.present()
-    
+
   }
 
 }
