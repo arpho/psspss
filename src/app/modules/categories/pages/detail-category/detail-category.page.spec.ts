2054f7dc49f2d1566ee5a46a73f7c2ac671c94b1
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-23 16:16:51 +0200

    initialize firebase

--- src/app/modules/categories/pages/detail-category/detail-category.page.spec.ts
+++ src/app/modules/categories/pages/detail-category/detail-category.page.spec.ts
@@ -1,14 +1,18 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import * as firebase from 'firebase/app';
 import { DetailCategoryPage } from './detail-category.page';
 import { ModalController, AngularDelegate, NavParams, NavController } from '@ionic/angular';
 import { MockNavParams } from 'src/app/modules/item/pages/selector-items/mockNavParams';
 import { HttpClientModule } from '@angular/common/http';
 import { CategoriesService } from '../../services/categorie.service';
+import { configs } from 'src/app/configs/credentials';
 
 
 describe('DetailCategoryPage', () => {
+  if(firebase.default.apps.length===0){
+    firebase.default.initializeApp(configs.firebase)
+  }
   let component: DetailCategoryPage;
   let fixture: ComponentFixture<DetailCategoryPage>;
 
