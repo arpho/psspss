3bb4c2a28602d974c61221932e0d3699171cd4b8
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-07-29 17:58:31 +0200

    putting field on manageUser

--- src/app/modules/user/pages/manage-user/manage-user.module.ts
+++ src/app/modules/user/pages/manage-user/manage-user.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ManageUserPageRoutingModule } from './manage-user-routing.module';
 
 import { ManageUserPage } from './manage-user.page';
+import { DynamicFormModule } from 'src/app/modules/dynamic-form/dynamic-form.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ManageUserPageRoutingModule
+    ManageUserPageRoutingModule,
+    DynamicFormModule
   ],
   declarations: [ManageUserPage]
 })
--- src/app/modules/user/pages/manage-user/manage-user.page.ts
+++ src/app/modules/user/pages/manage-user/manage-user.page.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import { DateQuestion } from 'src/app/modules/dynamic-form/models/question-date';
+import { DropdownQuestion } from 'src/app/modules/dynamic-form/models/question-dropdown';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { SwitchQuestion } from 'src/app/modules/item/models/question-switch';
+import { DateModel } from '../../models/birthDateModel';
 import { UserModel } from '../../models/userModel';
+import {configs} from '../../../../configs/credentials'
 
 @Component({
   selector: 'app-manage-user',
@@ -9,20 +15,79 @@ import { UserModel } from '../../models/userModel';
 })
 export class ManageUserPage implements OnInit {
 title= "gestione Utente"
+
 user:UserModel
+userFields= []
   constructor(public modalCtrl:ModalController,
     public navparams:NavParams,
     ) { }
 
-    dismiss() {
-      this.modalCtrl.dismiss()
-    }
+   
   
 
   ngOnInit() {
     this.user = this.navparams.get('item')
     console.log('user',this.user)
     this.title = this.user.getTitle().value.toString()
+    this.userFields=[
+      new TextboxQuestion({
+        key: 'firstName',
+        label: 'nome',
+        value: this.user ? this.user.firstName : 'nome',
+        order: 1,
+        required: true
+      }),
+      new TextboxQuestion({
+        key: 'lastName',
+        label: 'cognome',
+        value: this.user ? this.user.lastName : 'cognome',
+        order: 2
+      }),
+      new SwitchQuestion({
+        key: 'enabled',
+        label: 'abilitato',
+        value: this.user ? this.user.enabled : false,
+        labelTrue: 'utente  abilitato',
+        labelFalse: ' utente non abilitato ',
+        iconTrue: 'happy',
+        iconFalse: 'remove-circle',
+        order: 3
+      }),
+      new DateQuestion({
+        key: 'birthDate',
+        label: 'Data di nascita',
+        required: true,
+        value: this.user ? new DateModel(this.user.birthDate).formatDate() : new DateModel(new Date()), // "1977-03-16",
+        order: 4
+      }),
+      new DropdownQuestion({
+        key: 'level',
+        label: 'Ruolo utente',
+        options: configs['accessLevel'],
+        value:this.user? this.user.level:3
+      }),
+      new SwitchQuestion({
+        key: 'offlineEnabled',
+        label: 'supporto offline ',
+        value: this.user ? this.user.enabled : false,
+        labelTrue: 'supporto offline ok',
+        labelFalse: ' solo cloud ',
+        iconTrue: 'cloud-upload',
+        iconFalse: 'cloud',
+        order: 3
+      }),]
+  }
+
+  dismiss() {
+    this.modalCtrl.dismiss()
+  }
+
+  filter(ev){
+    console.log('filter',ev)
+  }
+
+  submit(ev){
+    console.log('submit',ev)
   }
 
 }
